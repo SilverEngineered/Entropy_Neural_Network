6452f1a1d63b4eba06be5f4e4cf1ab189d10c323
SilverEngineered <Daniel@SilverEngineered.com>
2019-04-21 00:35:44 -0400

    Finished preprocessing ... for now

---
+++ mainPreProcess.py
@@ -0,0 +1,17 @@
+from preProcess import *
+import os
+
+save_path = "../data/"
+read_path = "./winequality-red.csv"
+if not os.path.exists(save_path):
+        os.mkdir(save_path)
+
+features, labels = readDataFromCSV(read_path)
+train_features, test_features, validation_features = splitData(features)
+train_labels, test_labels, validation_labels = splitData(labels)
+writeToCSV(train_features,save_path + "train_features.csv")
+writeToCSV(test_features,save_path + "test_features.csv")
+writeToCSV(validation_features,save_path + "valid_features.csv")
+writeToCSV(train_labels,save_path + "train_labels.csv")
+writeToCSV(test_labels,save_path + "test_labels.csv")
+writeToCSV(validation_labels,save_path + "valid_lables.csv")
